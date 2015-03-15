29b6f1281eb75599f98ddccd6d790b0746bdd76b
muztaba <hasanat.muztaba@outlook.com>
2015-03-15 20:54:29 +0600

    Bring change in the gibbs algorithm.

--- src/algorithm/Gibbs.java
+++ src/algorithm/Gibbs.java
@@ -12,23 +12,56 @@ import java.util.List;
  * again. Rather it make a small change in the sample.
  */
 public class Gibbs extends Algorithm {
+
+    Undo undo;
+
+    class Undo {
+        int i, j;
+        int froward, backward;
+
+        Undo(int i, int j, int froward, int backward) {
+            this.i = i;
+            this.j = j;
+            this.froward = froward;
+            this.backward = backward;
+        }
+    }
+
     public void gibbs(List<Node> graph, int iteration) {
         // Initializing the variable.
         initVariable(graph.size());
 
         initConnectionMatrix(graph);
+
+        // For the starting up Gibbs algorithm this method provide a valid graph
+        // that satisfy the flow constraints.
+        validGraph(graph);
+
+        // Here begin the Gibbs algorithm.
         for (int mainLoop = 0; mainLoop < iteration; mainLoop++) {
+            // Make a single change in the flowMatrix.
+            makeChange(graph);
+            if (!checkValidity(graph)) {
+                undo();
+            }
+            int totalLoadShedding = totalLoadShedding(graph);
+            System.out.println(totalLoadShedding);
+        }
+    }
+
+    /**
+     * Give the Gibbs algorithm a valid graph. This method randomly generate flow then check the constraints.
+     * If the graph stable with the generated flow then it return from the method to further proceed.
+     * This give the Gibbs algorithm a valid graph for the start up.
+     */
+    protected void validGraph(List<Node> graph) {
+        while (true) {
+
+            // Generating flow between two node randomly within their edge capacity
             randomFlowGeneration(graph);
 
-            // This make sure that the given graph is valid.
-            if (!checkValidity(graph)) continue;
-            for (int i = 0; i < iteration; i++) {
-                makeChange(graph);
-                if (!checkValidity(graph)) break;
-                int totalLoadShedding = totalLoadShedding(graph);
-                System.out.println(totalLoadShedding);
-            }
-            System.out.println();
+            // If there is a valid graph with flow constraint then return from this method to further proceed.
+            if (checkValidity(graph)) return;
         }
     }
 
@@ -41,12 +74,26 @@ public class Gibbs extends Algorithm {
             int i = random.nextInt(connectionMatrix.length);
             int j = random.nextInt(connectionMatrix.length);
             if (connectionMatrix[i][j]) {
+
                 int index = indexOf(graph.get(i).edges(), j);
                 int randomCapacity = random.nextInt(graph.get(i).edges().get(index).getCapacity());
                 flowMatrix[i][j] = -randomCapacity;
                 flowMatrix[j][i] = randomCapacity;
+
+                undo = new Undo(i, j, flowMatrix[i][j], flowMatrix[j][i]);
+
                 break;
+
             }
         }
+    }
+
+    /**
+     * If the graph is not valid with the generated flow then this method undo the all change have been made
+     * in the flowMatrix.
+     */
+    protected void undo() {
+        flowMatrix[undo.i][undo.j] = undo.froward;
+        flowMatrix[undo.j][undo.i] = undo.backward;
     }
 }
