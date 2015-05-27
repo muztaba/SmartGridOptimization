eca8e0cc999fa7bf8c45202700b3c8b560b486b2
muztaba <hasanat.muztaba@gmail.com>
2015-05-27 19:33:07 +0600

    aco for single power source

---
+++ src/algorithm/Run.java
@@ -0,0 +1,8 @@
+package algorithm;
+
+/**
+ * Created by seal on 5/25/15.
+ */
+public interface Run {
+    public void run();
+}
---
+++ src/algorithm/aco/test/Ant.java
@@ -0,0 +1,18 @@
+package algorithm.aco.test;
+
+import java.util.List;
+
+/**
+ * Created by seal on 5/25/15.
+ */
+public class Ant {
+    private int power;
+    private List<Integer> visited;
+
+    public void nextNode(List<Node> list) {
+        int nextU;
+        int p;
+        for (Node node : list) {
+        }
+    }
+}
