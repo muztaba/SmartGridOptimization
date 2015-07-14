1f617e4a2b4ca97c4a32a7bd4e21ccba06772f48
muztaba <hasanat.muztaba@gmail.com>
2015-07-14 10:39:34 +0600

    working on graph class

--- src/graph/version2/Graph.java
+++ src/graph/version2/Graph.java
@@ -1,7 +1,41 @@
 package graph.version2;
 
+import Utils.Pair;
+import graph.vertex.Node;
+
+import java.util.*;
+
 /**
  * Created by seal on 7/13/15.
  */
-public class Graph {
+public class Graph <V extends Node, E> {
+    Map<Integer, V> vertexes = new HashMap<>();
+    Map<Integer, Set<Pair<Integer, Double>>> edges = new HashMap<>();
+
+    // Keep the list of the sources.
+    Set<Integer> sourceList = new HashSet<>();
+
+    public void addVertex(final V node) {
+        int nodeNumber = node.getNodeNumber();
+        vertexes.put(nodeNumber, node);
+        if (node.getSupply_demand() > 0) {
+            sourceList.add(nodeNumber);
+        }
+    }
+
+    public void addEdge(int u, int v, double capacity) {
+        Pair<Integer, Double> U = Pair.makePair(v, capacity);
+        Pair<Integer, Double> V = Pair.makePair(u, capacity);
+
+        if (!edges.containsKey(u)) {
+            edges.put(u, new HashSet<>());
+        }
+        if (!edges.containsKey(v)) {
+            edges.put(v, new HashSet<>());
+        }
+
+        edges.get(u).add(U);
+        edges.get(v).add(V);
+    }
+
 }
