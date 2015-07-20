f864647866b1ba542a3262d58b6f5bf03f34c886
muztaba <hasanat.muztaba@gmail.com>
2015-07-20 11:42:07 +0600

    restructure

--- src/graph/GraphInput.java
+++ src/graph/GraphInput.java
@@ -1,14 +1,16 @@
 package graph;
 
+import IOUtils.InputReader;
+import graph.version2.Graph;
+import graph.vertex.Node;
+
+import java.io.File;
+import java.io.IOException;
+
+
 /**
  * Created by seal on 3/7/15.
  */
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.StringTokenizer;
 
 /**
  *  This class is responsible for input graph from a file.
@@ -16,94 +18,27 @@ import java.util.StringTokenizer;
  *  Positive value mean supply and negative value for demand.
  */
 public class GraphInput {
-    List<Node> graph;
+    Graph graph;
 
-    public List<Node> readGraph(String path) throws IOException {
-        InputReader in = new InputReader(new File(path));
-        System.out.println("Starting graph input");
-        int nodeNumber = in.nextInt();
-        int edges = in.nextInt();
-        this.graph = new ArrayList<>(nodeNumber);
+    public Graph readGraph(String path) throws IOException {
+        InputReader reader = new InputReader(new File("input/graph.txt"));
+        this.graph = new Graph();
+        int nodeNumber = reader.nextInt();
+        int edgeNumber = reader.nextInt();
 
         for (int i = 0; i < nodeNumber; i++) {
-            double use = in.nextDouble();
-            graph.add(new Node(i, use));
+            int use = reader.nextInt();
+            graph.vertex.Node node = new Node(i, use);
+            graph.addVertex(node);
         }
 
-        for (int i = 0; i < edges; i++) {
-            int u = in.nextInt();
-            int v = in.nextInt();
-            int capacity = in.nextInt();
-            graph.get(u).setConnectedWith(v, capacity);
+        for (int i = 0; i < edgeNumber; i++) {
+            int u = reader.nextInt();
+            int v = reader.nextInt();
+            double capacity = reader.nextInt();
+            graph.addEdge(u, v, capacity);
         }
-        System.out.println("Finish input");
-        return graph;
+
+        return this.graph;
     }
-
-    class InputReader {
-        private BufferedReader reader;
-        public StringTokenizer tokenizer;
-
-        InputReader(File file) {
-            try {
-                reader = new BufferedReader(new FileReader(file));
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            tokenizer = null;
-        }
-
-        InputReader(InputStream stream) {
-            reader = new BufferedReader(new InputStreamReader(stream));
-            tokenizer = null;
-        }
-
-        public String next() {
-            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-                try {
-                    tokenizer = new StringTokenizer(reader.readLine());
-                } catch (Exception e) {
-
-                }
-            }
-            return tokenizer.nextToken();
-        }
-
-        public String nextLine() {
-            String str = "";
-            try {
-                str = reader.readLine();
-            } catch (IOException e) {
-                throw new RuntimeException();
-            }
-            return str;
-        }
-
-        public boolean hasMoreTokens() {
-            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-                String str = null;
-                try {
-                    str = reader.readLine();
-                } catch (IOException e) {
-                    throw new RuntimeException();
-                }
-                if (str == null) return false;
-                tokenizer = new StringTokenizer(str);
-            }
-            return true;
-        }
-
-        public int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        public double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        public long nextLong() {
-            return Long.parseLong(next());
-        }
-    }
-
 }
