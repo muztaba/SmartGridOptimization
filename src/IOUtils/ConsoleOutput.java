41bf53813b8c9bb482a4e4f80e6000ead2868478
muztaba <hasanat.muztaba@gmail.com>
2015-07-24 23:10:39 +0600

    init

---
+++ src/IOUtils/ConsoleOutput.java
@@ -0,0 +1,13 @@
+package IOUtils;
+
+/**
+ * Created by seal on 7/24/15.
+ */
+public class ConsoleOutput {
+    public static final boolean OUTPUT = true;
+    public static final boolean UNNECESSARY_OUTPUT = true;
+
+    public static void print(String str) {
+
+    }
+}
