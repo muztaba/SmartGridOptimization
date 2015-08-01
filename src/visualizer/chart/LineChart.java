c8d9771686092bae7159640eb12cac76d6595fbd
muztaba <hasanat.muztaba@gmail.com>
2015-08-02 02:20:17 +0600

    init

---
+++ src/visualizer/chart/LineChart.java
@@ -0,0 +1,16 @@
+package visualizer.chart;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.List;
+
+/**
+ * Created by seal on 8/2/15.
+ */
+public class LineChart extends Application {
+    @Override
+    public void start(Stage stage) {
+
+    }
+}
